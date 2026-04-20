0610ebb6f248888212f94c306bc66a6ed3b15d65
Tuong vy <hovuongtuongvy2711@gmail.com>
2026-04-20 11:02:00 +0700

    style: search

--- searchform.php
+++ searchform.php
@@ -10,8 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <form class="search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label class="screen-reader-text" for="cowm-search-field"><?php esc_html_e( 'Search for:', 'comeout-with-me' ); ?></label>
-	<input id="cowm-search-field" class="search-field" type="search" name="s" placeholder="<?php esc_attr_e( 'Tim truyen, review, tag...', 'comeout-with-me' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>"/>
-	<button class="search-submit" type="submit"><?php esc_html_e( 'Tim', 'comeout-with-me' ); ?></button>
+	<label class="screen-reader-text" for="cowm-search-field"><?php esc_html_e( 'Tìm kiếm:', 'comeout-with-me' ); ?></label>
+	<input id="cowm-search-field" class="search-field" type="search" name="s" placeholder="<?php esc_attr_e( 'Tìm truyện, review, tag...', 'comeout-with-me' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>"/>
+	<button class="search-submit" type="submit"><?php esc_html_e( 'Tìm', 'comeout-with-me' ); ?></button>
 </form>
-
